1cf8c46055b710267619981d5555fd7cc431b8ee
Yashwant Vaddineni <yashwant.v@techaspect.com>
2018-09-26 21:54:39 +0530

    Added Source Code for Challenge 1D Arrays.

---
+++ Day_5/arraysds.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the reverseArray function below.
+function reverseArray(a) {
+    
+    
+    var b= [a.length];
+        var t=a.length-1;
+        for(var i=0;i<a.length;i++){
+            b[t]=a[i];
+            t--;
+        }
+        return b;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}
