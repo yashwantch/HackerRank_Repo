a15d0d094fa1631f0bec33c5ddd19a7c1b207bc5
Yashwant Vaddineni <yashwant.v@techaspect.com>
2018-10-04 19:00:08 +0530

    Added Source Code for Challenge Birthday Chocolates.

---
+++ Day_10/birthdaychocolate.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the birthday function below.
+function birthday(s, d, m) {
+    
+    var count = 0;
+        for(var i=0;i<s.length;i++) {
+                    var sum = 0,k = 1;
+                    sum = sum + s[i];
+                for(var j=i+1; k <= m-1 && j<s.length ;j++, k++) {
+                        
+                    if(sum <= d) {
+                        sum = sum +s[j]; 
+                }
+                   
+            }
+            if(sum == d) {
+                count++;
+            }
+        }
+        return count;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const dm = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(dm[0], 10);
+
+    const m = parseInt(dm[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
