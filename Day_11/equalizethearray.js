af521c367fee9078ecb2c7dc77cfe9de12e7b7b7
Yashwant Vaddineni <yashwant.v@techaspect.com>
2018-10-06 19:52:00 +0530

    Added Source Code for Challenge Equalize the array.

---
+++ Day_11/equalizethearray.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the equalizeArray function below.
+function equalizeArray(arr) {
+    
+        arr.sort();
+        var count = 0, j = 0;
+        var n = arr.length;
+        for(var i = 1; i<n; i++){
+            if(arr[j] != arr[i]){
+                j++;
+                count++;
+            }
+        }   
+        return count;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = equalizeArray(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
