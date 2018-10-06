faad20499f4492b3936cf779d15d569f7a290889
Yashwant Vaddineni <yashwant.v@techaspect.com>
2018-10-06 19:53:55 +0530

    Added Source Code for Challenge Jumping on the Clouds.

---
+++ Day_11/jumpingontheclouds.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+
+    var count = 0;
+        var i = 0;
+        while(i < c.length-1) {
+            if (i < c.length-2 && c[i+2] == 0) {
+                i++;
+            }
+            if(i != c.length - 1) {
+                count++;
+            }
+            i++;
+        }
+        return count;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
