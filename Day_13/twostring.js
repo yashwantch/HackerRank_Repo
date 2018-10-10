7145029d4a4724caff97e3946d7bfad98d17cb77
Yashwant Vaddineni <yashwant.v@tadigital.com>
2018-10-10 15:48:38 +0530

    Added Source Code for Challenge Two String.

---
+++ Day_13/twostring.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the twoStrings function below.
+function twoStrings(s1, s2) {
+    
+    
+        var set1 = new Set(s1);
+        var set2 = new Set(s2);
+        var set3 = new Set(s1);
+       
+    for (var item of set2) {
+        set3.add(item);
+    }    
+        
+        return set3.size == set1.size + set2.size ? "NO" : "YES";
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s1 = readLine();
+
+        const s2 = readLine();
+
+        let result = twoStrings(s1, s2);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
