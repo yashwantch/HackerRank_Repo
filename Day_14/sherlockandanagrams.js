1fe9377502a2e88c390423921ae12168904592ca
Yashwant Vaddineni <yashwant.v@tadigital.com>
2018-10-11 10:56:38 +0530

    Added SourceCode for Challenge Sherlock and Anagrams.

---
+++ Day_14/sherlockandanagrams.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sherlockAndAnagrams function below.
+function sherlockAndAnagrams(s) {
+    
+        var map = new Map();
+        var totalCount = 0;
+
+        for(var i = 0 ; i < s.length; i++) {
+            for(var j=i+1 ; j <= s.length; j++) {
+                var SubString = s.substring(i,j);
+
+                var chars = [...SubString];
+                chars.sort();
+                
+                SubString = String(chars);
+
+                if(map.has(SubString)) {
+                    var value = map.get(SubString);
+                    totalCount = totalCount + value;
+
+                    map.set(SubString, value+1);
+                } 
+                else {
+                map.set(SubString, 1);
+                }
+            }
+        }
+        return totalCount;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        let result = sherlockAndAnagrams(s);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
