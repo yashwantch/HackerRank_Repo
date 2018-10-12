01aa31660d951cec8c936479f10c0fc35c2fc4f6
Yashwant Vaddineni <yashwant.v@tadigital.com>
2018-10-12 09:59:33 +0530

    Added SourceCode for Challenge Intro to Tutorial challenges.

---
+++ Day_15/intrototutorialchallenges.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the introTutorial function below.
+function introTutorial(V, arr) {
+    
+     var result = 0;
+        for(var i = 0 ; i < arr.length ; i++) {
+            
+            if(V == arr[i]){
+                result = i;
+                break;
+            }
+        }
+
+
+        return result;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const V = parseInt(readLine(), 10);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = introTutorial(V, arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
