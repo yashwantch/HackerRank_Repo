f9ae46bb776ada96c05688727abd9f8816fcfb75
Yashwant Vaddineni <yashwant.v@tadigital.com>
2018-10-18 10:10:38 +0530

    Added Source Code for Power Game.

---
+++ Day_19/powergame.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the powersGame function below.
+ */
+function powersGame(n) {
+    if (n%8 == 0) return 'Second';
+    return 'First';
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine(), 10);
+
+        let result = powersGame(n);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
